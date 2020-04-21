df5435a938bc5ef20e04549de56de047ae339943
Ali Ameer <aliameer6n@gmail.com>
2020-04-21 09:36:26 +0200

    Add Server-Side-Includes (SSI) route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get(['/', '/monolith'], (req, res, next) => {
   res.render('index.twig', { title: 'Pattern - Monolith' });
 });
 
+router.get('/ssi', (req, res, next) => {
+  res.render('index-ssi.twig', { title: 'Pattern - Server-Side-Includes(SSI)' });
+});
+
 module.exports = router;
