e62392a32327028e89feb317f5f25574c9d8f7f0
Ali Ameer <aliameer6n@gmail.com>
2020-04-21 10:07:25 +0200

    Add Edge-Side-Includes (ESI) route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,31 @@ router.get(['/', '/monolith'], (req, res, next) => {
 });
 
 router.get('/ssi', (req, res, next) => {
-  res.render('index-ssi.twig', { title: 'Pattern - Server-Side-Includes(SSI)' });
+  res.render('index-ssi.twig', {
+    title: 'Pattern - Server-Side-Includes(SSI)',
+  });
+});
+
+router.get(
+  '/snippet/:microfrontend(user|carts|catalogue)',
+  (req, res, next) => {
+    const microfrontend = req.params.microfrontend;
+    res.render(`microfrontends/${microfrontend}/${microfrontend}.twig`);
+  }
+);
+
+router.get('/esi', (req, res, next) => {
+  const ESI = require('nodesi');
+  const esi = new ESI({});
+
+  res.render(
+    'index-esi.twig',
+    {
+      title: 'Pattern - Edge-Side-Includes(ESI)',
+      microfrontendBaseUrl: 'http://localhost:3000',
+    },
+    async (err, html) => res.send(await esi.process(html))
+  );
 });
 
 module.exports = router;
