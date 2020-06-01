1f44ef6c3ed032e360249db07153bc5c1bf3db5d
Ali Ameer <aliameer6n@gmail.com>
2020-06-01 12:45:26 +0200

    [WIP] Add API specification to the project

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = require('./swagger-options');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +30,19 @@ app.use('/helpers', helpersPatternRouter);
 app.use('/server-side', serverSidePatternsRouter);
 app.use('/client-side', clientSidePatternsRouter);
 app.use('/hybrid', hybridPatternsRouter);
+
+const docsUrl = '/docs';
+app.get(['', '/'], (req, res, next) => res.redirect(302, docsUrl));
+
+app.use(docsUrl, swaggerUi.serve);
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.get(
+  docsUrl,
+  swaggerUi.setup(specs, {
+    explorer: true,
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ swagger-options.js
@@ -0,0 +1,12 @@
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Microfrontend Patterns',
+      version: '1.0.0',
+    },
+  },
+  apis: [],
+};
+
+module.exports = options;
